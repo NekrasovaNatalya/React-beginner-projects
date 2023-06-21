56ddad25114c339504f82e1c62375c2e10fa2fb2
NN <43994113+NekrasovaNatalya@users.noreply.github.com>
2023-06-21 11:08:07 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import './index.scss';
+import { Success } from './components/Success';
+import { Users } from './components/Users';
+
+// Тут список пользователей: https://reqres.in/api/users
 
 function App() {
-  const [count, setCount] = React.useState(0);
+const [users, setUsers] = React.useState([]);
+const [isLoading, setLoading] = React.useState(true);
+const [searchValue, setSearchValue] = React.useState('');
 
-  const onClickPlus = () => {
-    setCount(count + 1);
-  };
+React.useEffect(() => {
+  fetch('https://reqres.in/api/users')
+  .then((res) => res.json())
+  .then((json) => {
+    setUsers(json.data);
+  }).catch((err) => {
+    console.warn(err);
+    alert('Ошибка при получении пользователей');
+  }).finally(() => {setLoading(false)});
+}, []);
 
-  const onClickMinus = () => {
-    setCount(count - 1);
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value);
   };
 
   return (
     <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>{count}</h1>
-        <button onClick={onClickMinus} className="minus">- Минус</button>
-        <button onClick={onClickPlus} className="plus">Плюс +</button>
-      </div>
+      <Users
+      onChangeSearchValue={onChangeSearchValue}
+      searchValue={searchValue}
+      items={users}
+      isLoading={isLoading} />
+      {/* <Success /> */}
     </div>
   );
 }
